fe52923fffd066d8cde2f6a909dc34ef46f1b595
Valery Semenenko <gearmobile@gmail.com>
2026-01-01 23:07:17 +0300

    feat: use conditional show form controls

--- src/app/components/subscribe-form/subscribe-form.model.ts
+++ src/app/components/subscribe-form/subscribe-form.model.ts
@@ -4,6 +4,9 @@ export interface Subscribe {
   firstName: string;
   lastName: string;
   email: string;
+  isEmailRequired: boolean;
+  phone: string;
+  isPhoneRequired: boolean;
   yearsAsFun: number;
 }
 
@@ -11,6 +14,9 @@ export const subscribeFormDefaults: Subscribe = {
   firstName: '',
   lastName: '',
   email: '',
+  isEmailRequired: false,
+  phone: '',
+  isPhoneRequired: false,
   yearsAsFun: NaN,
 };
 
@@ -18,9 +24,17 @@ export const subscribeSchema = schema<Subscribe>((path) => {
   required(path.firstName, { message: 'First name field is required' });
   required(path.lastName, { message: 'Last name field is required' });
 
-  required(path.email, { message: 'Email name field is required' });
+  required(path.email, {
+    when: ({ valueOf }) => valueOf(path.isEmailRequired),
+    message: 'Email field is required',
+  });
   email(path.email, { message: 'Enter a valid email' });
   minLength(path.email, 6, { message: 'Email must at least be 6 characters' });
+
+  required(path.phone, {
+    when: ({ valueOf }) => valueOf(path.isPhoneRequired),
+    message: 'Phone field is required',
+  });
 
   min(path.yearsAsFun, 0, { message: 'Years can not be negative' });
   max(path.yearsAsFun, 100, { message: 'Enter valid number of years' });
