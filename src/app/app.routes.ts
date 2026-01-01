6cc0c32ae4b93b4efd51bdc1fbac505f9435c813
Valery Semenenko <gearmobile@gmail.com>
2026-01-01 19:13:16 +0300

    chore: init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'subscribe',
+    loadComponent: () =>
+      import('./components/subscribe-form/subscribe-form').then((m) => m.SubscribeForm),
+  },
+  {
+    path: '',
+    redirectTo: '/subscribe',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/subscribe',
+  },
+];
---
+++ src/app/components/subscribe-form/subscribe-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ef-subscribe-form',
+  imports: [],
+  templateUrl: './subscribe-form.html',
+  styleUrl: './subscribe-form.scss',
+})
+export class SubscribeForm {
+
+}
